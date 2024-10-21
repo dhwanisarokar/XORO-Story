482d468b54c2038ac0156b5729b12fe5ec4885bd
Nimesh2106 <nimeshsarokar2006@gmail.com>
2024-10-21 12:56:23 +0530

    BACKEND COMPLETE

---
+++ src/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const httpStatus = require("http-status");
+
+const { errorHandler } = require("./middlewares/error.middleware.js");
+const ApiError = require("./utils/ApiError.js");
+const router = require("./routes/task.routes.js");
+
+const app = express();
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api/tasks", router);
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
+
+// golab Error Middleware
+app.use(errorHandler);
+
+
+module.exports = app;
